628489761e406d24ede21220a60f935e4eaad0b1
TheMulhima <mulhima003@gmail.com>
2022-05-19 22:03:38 +0400

    Update to 1.5

---
+++ TranslationMod/Properties/Resources.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace TranslationMod.Properties
+{
+	// Token: 0x02000003 RID: 3
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x06000006 RID: 6 RVA: 0x0000219B File Offset: 0x0000039B
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000007 RID: 7 RVA: 0x000021A3 File Offset: 0x000003A3
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new ResourceManager("TranslationMod.Properties.Resources", typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000002 RID: 2
+		// (get) Token: 0x06000008 RID: 8 RVA: 0x000021CF File Offset: 0x000003CF
+		// (set) Token: 0x06000009 RID: 9 RVA: 0x000021D6 File Offset: 0x000003D6
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x17000003 RID: 3
+		// (get) Token: 0x0600000A RID: 10 RVA: 0x000021DE File Offset: 0x000003DE
+		internal static string Achievements
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Achievements", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000004 RID: 4
+		// (get) Token: 0x0600000B RID: 11 RVA: 0x000021F4 File Offset: 0x000003F4
+		internal static string Backer_Messages
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Backer_Messages", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000005 RID: 5
+		// (get) Token: 0x0600000C RID: 12 RVA: 0x0000220A File Offset: 0x0000040A
+		internal static string Banker
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Banker", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000006 RID: 6
+		// (get) Token: 0x0600000D RID: 13 RVA: 0x00002220 File Offset: 0x00000420
+		internal static string Charm_Slug
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Charm_Slug", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000007 RID: 7
+		// (get) Token: 0x0600000E RID: 14 RVA: 0x00002236 File Offset: 0x00000436
+		internal static string Cornifer
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Cornifer", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000008 RID: 8
+		// (get) Token: 0x0600000F RID: 15 RVA: 0x0000224C File Offset: 0x0000044C
+		internal static string CP2
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("CP2", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000009 RID: 9
+		// (get) Token: 0x06000010 RID: 16 RVA: 0x00002262 File Offset: 0x00000462
+		internal static string CP3
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("CP3", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000A RID: 10
+		// (get) Token: 0x06000011 RID: 17 RVA: 0x00002278 File Offset: 0x00000478
+		internal static string Credits_List
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Credits_List", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000B RID: 11
+		// (get) Token: 0x06000012 RID: 18 RVA: 0x0000228E File Offset: 0x0000048E
+		internal static string Dream_Witch
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Dream_Witch", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000C RID: 12
+		// (get) Token: 0x06000013 RID: 19 RVA: 0x000022A4 File Offset: 0x000004A4
+		internal static string Dreamers
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Dreamers", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000D RID: 13
+		// (get) Token: 0x06000014 RID: 20 RVA: 0x000022BA File Offset: 0x000004BA
+		internal static string Elderbug
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Elderbug", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000E RID: 14
+		// (get) Token: 0x06000015 RID: 21 RVA: 0x000022D0 File Offset: 0x000004D0
+		internal static string Enemy_Dreams
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Enemy_Dreams", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700000F RID: 15
+		// (get) Token: 0x06000016 RID: 22 RVA: 0x000022E6 File Offset: 0x000004E6
+		internal static string General
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("General", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000010 RID: 16
+		// (get) Token: 0x06000017 RID: 23 RVA: 0x000022FC File Offset: 0x000004FC
+		internal static string Ghosts
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Ghosts", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000011 RID: 17
+		// (get) Token: 0x06000018 RID: 24 RVA: 0x00002312 File Offset: 0x00000512
+		internal static string Hornet
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Hornet", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000012 RID: 18
+		// (get) Token: 0x06000019 RID: 25 RVA: 0x00002328 File Offset: 0x00000528
+		internal static string Hunter
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Hunter", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000013 RID: 19
+		// (get) Token: 0x0600001A RID: 26 RVA: 0x0000233E File Offset: 0x0000053E
+		internal static string Iselda
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Iselda", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000014 RID: 20
+		// (get) Token: 0x0600001B RID: 27 RVA: 0x00002354 File Offset: 0x00000554
+		internal static string Jiji
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Jiji", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000015 RID: 21
+		// (get) Token: 0x0600001C RID: 28 RVA: 0x0000236A File Offset: 0x0000056A
+		internal static string Journal
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Journal", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000016 RID: 22
+		// (get) Token: 0x0600001D RID: 29 RVA: 0x00002380 File Offset: 0x00000580
+		internal static string Lore_Tablets
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Lore_Tablets", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000017 RID: 23
+		// (get) Token: 0x0600001E RID: 30 RVA: 0x00002396 File Offset: 0x00000596
+		internal static string MainMenu
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("MainMenu", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000018 RID: 24
+		// (get) Token: 0x0600001F RID: 31 RVA: 0x000023AC File Offset: 0x000005AC
+		internal static string Map_Zones
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Map_Zones", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000019 RID: 25
+		// (get) Token: 0x06000020 RID: 32 RVA: 0x000023C2 File Offset: 0x000005C2
+		internal static string Minor_NPC
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Minor_NPC", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001A RID: 26
+		// (get) Token: 0x06000021 RID: 33 RVA: 0x000023D8 File Offset: 0x000005D8
+		internal static string Nailmasters
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Nailmasters", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001B RID: 27
+		// (get) Token: 0x06000022 RID: 34 RVA: 0x000023EE File Offset: 0x000005EE
+		internal static string Nailsmith
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Nailsmith", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001C RID: 28
+		// (get) Token: 0x06000023 RID: 35 RVA: 0x00002404 File Offset: 0x00000604
+		internal static string Prompts
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Prompts", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001D RID: 29
+		// (get) Token: 0x06000024 RID: 36 RVA: 0x0000241A File Offset: 0x0000061A
+		internal static string Quirrel
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Quirrel", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001E RID: 30
+		// (get) Token: 0x06000025 RID: 37 RVA: 0x00002430 File Offset: 0x00000630
+		internal static string Relic_Dealer
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Relic_Dealer", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001F RID: 31
+		// (get) Token: 0x06000026 RID: 38 RVA: 0x00002446 File Offset: 0x00000646
+		internal static string Shaman
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Shaman", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000020 RID: 32
+		// (get) Token: 0x06000027 RID: 39 RVA: 0x0000245C File Offset: 0x0000065C
+		internal static string Sly
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Sly", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000021 RID: 33
+		// (get) Token: 0x06000028 RID: 40 RVA: 0x00002472 File Offset: 0x00000672
+		internal static string Stag
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Stag", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000022 RID: 34
+		// (get) Token: 0x06000029 RID: 41 RVA: 0x00002488 File Offset: 0x00000688
+		internal static string StagMenu
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("StagMenu", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000023 RID: 35
+		// (get) Token: 0x0600002A RID: 42 RVA: 0x0000249E File Offset: 0x0000069E
+		internal static string Titles
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Titles", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000024 RID: 36
+		// (get) Token: 0x0600002B RID: 43 RVA: 0x000024B4 File Offset: 0x000006B4
+		internal static string UI
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("UI", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000025 RID: 37
+		// (get) Token: 0x0600002C RID: 44 RVA: 0x000024CA File Offset: 0x000006CA
+		internal static string Zote
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Zote", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x04000003 RID: 3
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000004 RID: 4
+		private static CultureInfo resourceCulture;
+	}
+}
---
+++ TranslationMod/TranslationMod.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Language;
+using Modding;
+
+namespace TranslationMod
+{
+	public class TranslationMod : Mod, ITogglableMod
+	{
+		public Dictionary<string, Dictionary<string, string>> translationDict;
+		public override string GetVersion() => "1.0.1";
+
+		public TranslationMod()
+		{
+			InitializeDictionaries();
+			ModHooks.LanguageGetHook += LanguageGetHook;
+		}
+
+		public override void Initialize()
+		{
+			Log("Initializing Poorly Translated Mod");
+		}
+
+		public void Unload()
+		{
+			ModHooks.LanguageGetHook -= LanguageGetHook;
+		}
+
+		private string LanguageGetHook(string key, string sheettitle, string orig)
+		{
+			if (translationDict.ContainsKey(sheettitle))
+			{
+				if (translationDict[sheettitle].TryGetValue(key, out var newText))
+				{
+					return newText;
+				}
+				else
+				{
+					LogWarn($"Not Found {key}:{orig}");
+				}
+			}
+			return orig;
+		}
+
+		private void InitializeDictionaries()
+		{
+			translationDict = new Dictionary<string, Dictionary<string, string>>();
+			Assembly executingAssembly = Assembly.GetExecutingAssembly();
+			foreach (string text in executingAssembly.GetManifestResourceNames())
+			{
+				LogDebug(text);
+				if (text.EndsWith("txt"))
+				{
+					Dictionary<string, string> dictionary = new Dictionary<string, string>();
+					StreamReader streamReader = new StreamReader(executingAssembly.GetManifestResourceStream(text));
+					string text2;
+					while ((text2 = streamReader.ReadLine()) != null)
+					{
+						dictionary.Add(text2.Split(new char[]
+						{
+							':'
+						})[0].Trim(), text2.Split(new char[]
+						{
+							':'
+						})[1].Trim());
+					}
+					streamReader.Close();
+					translationDict.Add(Path.GetFileNameWithoutExtension(text).Remove(0, 25), dictionary);
+				}
+			}
+		}
+	}
+}
